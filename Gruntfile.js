1ea37b6d25992e617c6f7b5b919c420383955f24
Rafael Nowrotek <mail@benignware.com>
2015-07-05 23:53:11 +0200

    Added grunt-livemd

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,9 @@ module.exports = function(grunt) {
     copy: {
       dist: {
         expand: true, cwd: 'src/', src: ['**'], dest: 'dist/'
+      },
+      site: {
+        expand: true, cwd: 'dist/', src: ['**/*'], dest: 'site/'
       }
     },
     // Lint definitions
@@ -39,6 +42,30 @@ module.exports = function(grunt) {
           'dist/visualist.js': 'src/visualist.js'
         }
       }
+    },
+    livemd: {
+      options: {
+        prefilter: function(string) {
+          return string.replace(grunt.config().pkg && grunt.config().pkg.homepage && new RegExp("\\[.*\\]\\(" + grunt.config().pkg.homepage.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") + "\\)", "gi"), "");
+        }
+      },
+      site: {
+        files: {
+          'site/index.html': ['README.md']
+        }
+      }
+    },
+    'gh-pages': {
+      options: {
+        // Options for all targets go here.
+      },
+      site: {
+        options: {
+          base: 'site'
+        },
+        // These files will get pushed to the `gh-pages` branch (the default).
+        src: ['**/*']
+      }
     }
   });
   
@@ -46,6 +73,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-qunit");
+  grunt.loadNpmTasks("grunt-livemd");
+  grunt.loadNpmTasks("grunt-gh-pages");
   
   grunt.loadNpmTasks("grunt-browserify");
 
@@ -54,4 +83,6 @@ module.exports = function(grunt) {
   grunt.registerTask('test', ['jshint', 'build', 'qunit']);
   
   grunt.registerTask('default', ['build']);
+  
+  grunt.registerTask('site', ['build', 'copy:site', 'livemd:site']);
 };
